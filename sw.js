bf42b64afadd448ab58a293cbe0f80f908a08510
AutopecasRoniJoao <joaomarcelloigle@gmail.com>
2026-08-14 10:50:12 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,41 @@
+const CACHE_NAME = 'ponto-roni-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './admin.html',
+  './manifest.json',
+  'https://roni.com.br/wp-content/uploads/2023/02/Logomarca-RONI-oficial.jpg'
+];
 
+// Instalação do Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Ativação e limpeza de caches antigos
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Interceptação de requisições (Carrega offline)
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
